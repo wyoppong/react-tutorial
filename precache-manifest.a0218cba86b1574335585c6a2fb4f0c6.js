57c82f13af3c5e4fde65aabc19192a0610ab4e22
WY OPPONG <wyoppong@gmail.com>
2019-09-14 13:54:04 +0000

    Updates

---
+++ precache-manifest.a0218cba86b1574335585c6a2fb4f0c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6787243c9b51fcd14bbd5b8ae938d097",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2a43280d65eae1a71eee",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "1daee68de89d848c2d08",
+    "url": "/react-tutorial/static/js/2.b97002d1.chunk.js"
+  },
+  {
+    "revision": "2a43280d65eae1a71eee",
+    "url": "/react-tutorial/static/js/main.5b624d70.chunk.js"
+  },
+  {
+    "revision": "3edcf17ea693c13889ea",
+    "url": "/react-tutorial/static/js/runtime~main.3e370783.js"
+  }
+]);
